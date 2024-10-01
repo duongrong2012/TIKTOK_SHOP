c0bf428cee20580486778311d35aee5511b017b8
duongrong2012 <67502432+duongrong2012@users.noreply.github.com>
2024-10-01 20:55:27 +0700

    Initialize repository

---
+++ src/app/assets/index.ts
@@ -0,0 +1,27 @@
+import Group19624 from './Group 19624.svg'
+import arrowLeft from './arrowLeft.svg'
+import arrowleft2 from './arrow-left.svg'
+import shop from './shop.svg'
+import cloudConnection from './cloud-connection.svg'
+import boxAdd from './box-add.svg'
+import bag2 from './bag-2.svg'
+import setting3 from './setting-3.svg'
+import box from './box.svg'
+import arrowDown from './arrow-down.svg'
+import setting from './setting.svg'
+
+export const images = {
+    Group19624,
+    arrowLeft,
+    arrowleft2,
+    shop,
+    cloudConnection,
+    boxAdd,
+    bag2,
+    setting3,
+    box,
+    arrowDown,
+    setting
+}
+
+export default images
